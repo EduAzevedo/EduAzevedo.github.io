592a8e21bcb91638cbd38afc1ccb5b3f622aa84e
Eduardo Azevedo <89713966+EduAzevedo@users.noreply.github.com>
2023-04-24 10:38:14 -0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,9 +29,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "72b3566635b4631189276233e95b8d0f",
-"/": "72b3566635b4631189276233e95b8d0f",
-"main.dart.js": "1d5b455dc3374936ed92d439e6ec94ee",
+"index.html": "0ddf84548a314fa332cb72b943a13a79",
+"/": "0ddf84548a314fa332cb72b943a13a79",
+"main.dart.js": "aeb98fdf38775867e87a1a2d74aba720",
 "manifest.json": "e5b463f8e8f895e285d1250a35c953b9",
 "version.json": "8bd1e61dc6647d1a1d24b140c22caea3"
 };
